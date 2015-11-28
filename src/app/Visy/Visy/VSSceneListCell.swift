e1de52039a289bc3b640074ee89dc6a57d28f9b0
Yoshiyuki Kuga <yoshiyukikuga@Yossan-MacBook-Air.local>
2015-11-29 00:03:25 +0900

    VSSceneListCell is created

---
+++ src/app/Visy/Visy/VSSceneListCell.swift
@@ -0,0 +1,24 @@
+//
+//  VSSceneListCell.swift
+//  Visy
+//
+//  Created by Yoshiyuki Kuga on 2015/11/28.
+//  Copyright © 2015年 TK_15. All rights reserved.
+//
+
+import UIKit
+
+class VSSceneListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
